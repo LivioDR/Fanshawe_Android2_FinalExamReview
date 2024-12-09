be815fac927b2280a52a6a09abf7fc1cd2744de0
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-12-09 17:22:45 -0500

    initial commit firebase auth integrated viewBinding enabled

---
+++ app/src/main/java/com/example/livioreinoso1165606/finalexamreview/view/LoginActivity.kt
@@ -0,0 +1,77 @@
+package com.example.livioreinoso1165606.finalexamreview.view
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.livioreinoso1165606.finalexamreview.R
+import com.example.livioreinoso1165606.finalexamreview.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.username.text.toString()
+            val password = binding.password.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("firebase", "signInWithEmail:success")
+                        val user = auth.currentUser
+//                        updateUI(user)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication succeeded for user ${auth.currentUser?.uid}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("firebase", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+//                        updateUI(null)
+                    }
+                }
+        }
+
+
+    }
+
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            // TODO: move the user to the next screen
+        }
+    }
+}
